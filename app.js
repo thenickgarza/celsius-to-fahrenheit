a41ccea02a51bb42057f89faa682341509791432
Nick Garza <thenickgarza14@gmail.com>
2022-08-20 15:58:38 -0500

    added formula to calculate celsius to newton

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // Variable for kelvin temp
-const kelvin = 300
+const kelvin = 293
 // formula for celsius 
 const celsius = kelvin - 273
 // rounds up the number
 const fahrenheit = Math.floor(celsius * (9/5) + 32)
 
 console.log(`The tempature is ${fahrenheit} deggrees Fahrenheit`)
+
+// Formula for Newton 
+const Newton = Math.floor(celsius * (33/100))
+
+console.log(`The tempature in Newton is ${Newton}`)
